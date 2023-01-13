d4a821d1296eb4e9530e7a08535d6616b6f0468b
Welber Santos <111401566+welberos@users.noreply.github.com>
2023-01-13 16:26:23 -0300

    update files

---
+++ script.js
@@ -0,0 +1,39 @@
+function carregar () {
+  var msg = window.document.getElementById('msg')
+  var img = window.document.getElementById('imagem')
+  var data = new Date()
+  var hora = data.getHours()
+  var min = data.getMinutes()
+ 
+
+  if (hora <= 9 ) {
+    hora = '0' + hora 
+  }
+
+  if (min <= 9) {
+  min = '0'+ min
+  }
+  
+  msg.innerHTML = `<p>Agora são <strong>${hora}</strong> horas <strong>${min}</strong> minutos.</p>`
+  
+  if (hora >= 0 && hora < 12) {
+    msg.innerHTML+='Bom dia!'
+    img.src  = "./img/manhã.png"
+    document.body.style.background = '#dbaf76'
+    if (hora <= 9) {
+      hora = '0' + hora 
+    }
+    
+  } else if (hora >= 12 && hora < 18) {
+    msg.innerHTML+='Boa tarde!'
+    img.src  = "./img/tarde.png"
+    document.body.style.background = '##cd6f1d'
+  } else {
+    msg.innerHTML+='Boa Noite!'
+    img.src  = "./img/noite.png"
+    document.body.style.background = '#362525'
+  }
+  
+  
+
+}
